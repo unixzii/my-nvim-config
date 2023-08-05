eebc7c8073df7a474b40b5daf4c36e0ec692b8f8
Cyandev <unixzii@gmail.com>
2023-08-05 14:00:40 +0800

    Add options for Neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,21 @@ vim.o.scrolloff = 10
 
 vim.o.spell = true
 vim.o.spelllang = "en_us"
+
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+  vim.opt.linespace = 2
+  vim.g.neovide_confirm_quit = true
+
+  local alpha = function()
+    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+  end
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.97
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+
+  vim.g.neovide_cursor_animation_length = 0.1
+  vim.g.neovide_cursor_trail_size = 0.4
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_cursor_vfx_particle_density = 30.0
+end
